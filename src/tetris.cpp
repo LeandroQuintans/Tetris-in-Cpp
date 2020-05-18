74c945df048f7f6643354a93a0d5b76f320c3181
Leandro Quintans <44684615+LeandroQuintans@users.noreply.github.com>
2020-05-18 22:07:23 +0100

    Tetris::canPieceBePlaced() implemented

--- src/tetris.cpp
+++ src/tetris.cpp
@@ -7,4 +7,25 @@ namespace tetris {
             m_pieceBuffer.push(Piece::getRandomPiece());
         m_currentPiece = Piece::getRandomPiece();
     }
+
+    bool Tetris::canPieceBePlaced(const Piece& piece, const Coords2D& position) const {
+        std::size_t pieceFormHeight = piece.currentFormation().getHeight();
+        std::size_t pieceFormWidth = piece.currentFormation().getWidth();
+
+        for (std::size_t i = 0; i < pieceFormHeight; ++i) {
+            for (std::size_t j = 0; j < pieceFormWidth; ++j) {
+                if (piece.currentFormation().view(i, j)) {
+                    if (i + position.first >= m_playfield.getHeight() 
+                     || j + position.second >= m_playfield.getWidth()) {
+                        return false;
+                    }
+                    else if (m_playfield.view(i + position.first, j + position.second)) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
 }
