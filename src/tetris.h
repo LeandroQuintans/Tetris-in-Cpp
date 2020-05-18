34f8908b2f50c9289949b2d085e2ee151440d5fe
Leandro Quintans <44684615+LeandroQuintans@users.noreply.github.com>
2020-05-18 03:24:08 +0100

    Draft of the necessary methods for the Tetris class

--- src/tetris.h
+++ src/tetris.h
@@ -1,11 +1,39 @@
 #ifndef TETRIS_H
 #define TETRIS_H
 
+#include <queue>
+#include <iostream>
+
 #include "matrix.h"
 #include "piece.h"
 
 namespace tetris {
-    
+
+    class Tetris {
+    public:
+        typedef mycontainers::Matrix<int, 10, 20> Playfield;
+
+    public:
+        Playfield m_playfield;
+        int m_linesCleared = 0;
+        std::queue<Piece> m_pieceBuffer;
+        Piece m_currentPiece;
+
+        movePiece(int direction);
+
+    public:
+        Tetris();
+
+        movePieceLeft();
+        movePieceRight();
+        rotatePieceClockwise();
+        rotatePieceCounterClockwise();
+        softDropPiece();
+        hardDropPiece();
+        clearLines();
+
+        gameLoop();
+    };
 }
 
 #endif
